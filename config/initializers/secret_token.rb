1d00fbcb9e90f77861431773732984301d03471a
Lindsay Nauman <lindsay.nauman@gmail.com>
2013-09-16 15:49:23 -0400

    Initial Commit and delete minitest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parking::Application.config.secret_key_base = '83cb48e9234bb87f90b1aeca7129eeb22fd9d076c707a537c4a0f59701779640475423b58ce745e5188fc9be6deb250ee13158f349280a668cb0249b83a17c26'
